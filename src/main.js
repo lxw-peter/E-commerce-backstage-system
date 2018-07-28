99b957a6c896325474283e2be6fc198619116e08
lxw-peter <1270542959@qq.com>
2018-07-28 14:58:23 +0800

    登陆界面完成

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
-import '@styles/index.scss'
+import 'element-ui/lib/theme-chalk/index.css'
+import './styles/index.scss'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
